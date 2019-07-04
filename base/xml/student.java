d081b692270a8d8737687ea2c98124f9b760636d
sthgreat <37435108+sthgreat@users.noreply.github.com>
2019-07-04 19:57:00 +0800

    Create student.java

---
+++ base/xml/student.java
@@ -0,0 +1,31 @@
+package cn.itcast.vo;
+
+public class Student {
+    private String id;
+    private String name;
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
